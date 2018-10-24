e3c0eb22b45943bab7da051c3d4c9e7465e28a10
Daniel <daniel.97.molin@gmail.com>
2018-10-24 16:24:16 -0600

    Diagrama y Test

--- src/test/java/dsi235/controller/UsuarioSessionTest.java
+++ src/test/java/dsi235/controller/UsuarioSessionTest.java
@@ -9,10 +9,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import dsi235.boundary.backingBeans.LoginSessionBean;
+import dsi235.controllers.TicketController;
 import dsi235.controllers.UsuarioController;
+import dsi235.entities.Estado;
+import dsi235.entities.Ticket;
 import dsi235.entities.Usuario;
 
 import static org.junit.Assert.*;
+
+import java.util.Date;
 
 
 @RunWith(SpringRunner.class)
@@ -22,11 +27,21 @@ public class UsuarioSessionTest {
 	
 	private UsuarioController usuarioController;
 	private LoginSessionBean sessionBean;
+	private TicketController ticketController;
 	
 	@Test
 	public void iniciarSesionCorrecto() {
 		Usuario user = usuarioController.autenticar("daniel97molin@gmail.com", "dmdm");
 		assertEquals(user.getIdUsuario(), Long.valueOf(4));
+	}
+	
+	@Test
+	public void crearTicket() {
+		Ticket ticket = new Ticket(null, "Prueba de ticket (Prueba de integracion)", new Date());
+		ticket.setIdEstado(new Estado(Short.valueOf("1")));
+		ticket.setIdUsuario(new Usuario(Long.valueOf(4)));
+		ticket = ticketController.save(ticket);
+		assertNotNull(ticket.getIdTicket());
 	}
 	
 
@@ -35,6 +50,15 @@ public class UsuarioSessionTest {
 	public LoginSessionBean getSessionBean() {
 		return sessionBean;
 	}
+	
+	public TicketController getTicketController() {
+		return ticketController;
+	}
+	@Autowired
+	public void setTicketController(TicketController ticketController) {
+		this.ticketController = ticketController;
+	}
+
 	@Autowired
 	public void setSessionBean(LoginSessionBean sessionBean) {
 		this.sessionBean = sessionBean;
