3667441b5769ce66c17a9426185a78323928f229
Doratt <luisdoratt@hotmail.com>
2018-10-30 17:10:16 -0600

    Arreglando cosas

--- src/main/java/dsi235/boundary/backingBeans/historialUsuarioBackingBean.java
+++ src/main/java/dsi235/boundary/backingBeans/historialUsuarioBackingBean.java
@@ -1,18 +1,26 @@
 package dsi235.boundary.backingBeans;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.context.annotation.SessionScope;
 
 import dsi235.controllers.TicketController;
 import dsi235.entities.Ticket;
 import dsi235.entities.Usuario;
+import dsi235.utilities.PageParser;
 
 @ManagedBean(value="historialUsuarioBackingBean")
 @SessionScope
@@ -26,6 +34,7 @@ public class historialUsuarioBackingBean implements Serializable{
 	private LoginSessionBean sessionBean;
 	private Page<Ticket> historialTicketUsuario;
 	private Page<Ticket> historialTicketTecnico;
+	private Usuario usuarioLogueado;
 	
 	
 	@PostConstruct
@@ -35,6 +44,89 @@ public class historialUsuarioBackingBean implements Serializable{
 		System.out.println(historialTicketUsuario);
 		historialTicketTecnico= tc.findCompletadosByEncargado(sessionBean.getUsuarioLogueado().getIdUsuario(), 1, 5);
 	}
+	
+	public void inicializarModelo() {
+		try {
+		model=new LazyDataModel<Ticket>() {
+			 @Override
+             public Ticket getRowData(String rowKey) {
+                 return obtenerRowData(rowKey);
+             }
+
+             @Override
+             public Object getRowKey(Ticket object) {
+                 return obtenerRowKey(object);
+             }
+             @Override
+             public List<Ticket> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters){
+                 return cargarDatos(first,pageSize, sortField, sortOrder, filters);
+             }
+		};
+		}catch (Exception ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+		}
+	}
+
+	   public Object obtenerRowKey(Ticket object) {
+	        if (object != null) {
+	            return object.getIdTicket();
+	        }
+	        return null;
+	    }
+	
+	   public Ticket obtenerRowData(String rowKey) {
+	        try {
+	            List<Ticket> registros = (List<Ticket>) this.model.getWrappedData();
+	            if (registros != null && !registros.isEmpty()) {
+	                Long buscado = Long.parseLong(rowKey);
+	                for (Ticket r : registros) {
+	                    if (r.getIdTicket().compareTo(buscado) == 0) {
+	                        return r;
+	                    }
+	                }
+	            }
+	        } catch (Exception ex) {
+	            Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+	        }
+	        return null;
+	    }
+
+	   
+	public List<Ticket> cargarDatos(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+        List<Ticket> salida = null;
+        Page<Ticket> page=null;
+        try {
+            if (this.tc != null) {
+            	System.out.println(first);
+            	page=this.tc.findNoasignadosBySucursal(usuarioLogueado.getIdSucursal().getIdSucursal(),this.estado.getIdEstado(),PageRequest.of(PageParser.parsePage(first, pageSize), pageSize));
+                salida = page.getContent();
+                if (this.model != null) {
+                    System.out.println(page.getTotalElements());
+                	this.model.setRowCount((Integer.valueOf(String.valueOf(page.getTotalElements()))));
+                    
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+        } finally {
+            if (salida == null) {
+                salida = Collections.emptyList();
+            }
+        }
+        return salida;
+    }
+	
+
+    public LazyDataModel<Ticket> getModel() {
+		return model;
+	}
+
+
+	public void setModel(LazyDataModel<Ticket> model) {
+		this.model = model;
+	}
+ip
+	
 	
 	public Page<Ticket> getHistorialTicketTecnico() {
 		return historialTicketTecnico;
--- src/main/java/dsi235/utilities/PageParser.java
+++ src/main/java/dsi235/utilities/PageParser.java
@@ -1,10 +1,7 @@
 package dsi235.utilities;
 
 public class PageParser {
-
 	public static Integer parsePage(int first, int pagesize) {
 		return ((first)/pagesize);
 	}
-	
-	
 }
