598f7fd8b6c50d65dfcc28890be7282f90c24cb9
Doratt <luisdoratt@hotmail.com>
2018-11-08 23:51:21 -0600

    Gestionar estados

--- src/main/java/dsi235/boundary/backingBeans/AsignacionesBackingBean.java
+++ src/main/java/dsi235/boundary/backingBeans/AsignacionesBackingBean.java
@@ -10,13 +10,17 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 
+import org.primefaces.PrimeFaces;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import dsi235.controllers.ComentarioController;
 import dsi235.controllers.TicketController;
+import dsi235.controllers.TicketEncargadoController;
 import dsi235.entities.Comentario;
 import dsi235.entities.Estado;
 import dsi235.entities.Ticket;
+import dsi235.entities.TicketEncargado;
+import dsi235.utilities.ESTADO;
 import dsi235.utilities.EstadosLoader;
 
 @ManagedBean(value = "asignacionesBackingBean")
@@ -26,25 +30,27 @@ public class AsignacionesBackingBean implements Serializable {
 	private static final long serialVersionUID = -9176707771226005862L;
 	private LoginSessionBean sessionBean;
 	private TicketController ticketController;
-	
+
 	private List<Ticket> tickets;
 	private Ticket ticketSeleccionado;
-	
+
 	private Comentario comentario;
 	private ComentarioController comentarioController;
 	private List<Comentario> comentarios;
-	
+
 	private EstadosLoader el;
 	private Short estadoSeleccionado;
-	
-	
+
+	private TicketEncargadoController tec;
+	private List<TicketEncargado> asignados;
+
 	@PostConstruct
 	public void init() {
-		this.tickets = ticketController.findNoCompletadosByEncargado(sessionBean.getUsuarioLogueado().getIdUsuario());   
+		this.tickets = ticketController.findNoCompletadosByEncargado(sessionBean.getUsuarioLogueado().getIdUsuario());
 		setComentario(new Comentario());
-		
+
 	}
-	
+
 	public void crearComentario() {
 
 		if (!comentario.getContenido().isEmpty()) {
@@ -65,16 +71,46 @@ public class AsignacionesBackingBean implements Serializable {
 			System.out.println("contenido isempty");
 		}
 	}
-	
-	public void mostrarEstadoSeleccionado() {
-		System.out.println(estadoSeleccionado);
-	}
-	
+
 	public void cargarComentarios() {
-		estadoSeleccionado=Short.valueOf(String.valueOf((ticketSeleccionado.getIdEstado().getIdEstado()-1)));
+		setEstadoSeleccionado(Short.valueOf(String.valueOf(ticketSeleccionado.getIdEstado().getIdEstado()-1)));
+		System.out.println(estadoSeleccionado);
 		setComentarios(comentarioController.findByIdTicket(ticketSeleccionado));
 	}
-	
+
+	public void modificarEstadoTicket() {
+		if (estadoSeleccionado != 0) {
+			if ((estadoSeleccionado+1) != ticketSeleccionado.getIdEstado().getIdEstado()) {
+				ticketSeleccionado.setIdUsuarioModificador(sessionBean.getUsuarioLogueado());
+				ticketSeleccionado.setFechaModificacion(new Date());
+				if (estadoSeleccionado == ESTADO.completado.value) {
+					ticketSeleccionado.setFechaCompletado(new Date());
+					asignados=tec.findByIdTicket_IdTicket(ticketSeleccionado.getIdTicket());
+					for (TicketEncargado ticketEncargado : asignados) {
+						ticketEncargado.setActivo(false);
+						ticketEncargado.setFechaModificacion(new Date());
+						ticketEncargado.setIdUsuarioModificador(sessionBean.getUsuarioLogueado());
+						try {
+							tec.save(ticketEncargado);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				} 
+					ticketSeleccionado.setIdEstado(el.get(estadoSeleccionado));
+					try {
+						ticketController.save(ticketSeleccionado);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					init();
+					PrimeFaces current = PrimeFaces.current();
+					current.executeScript("PF('infoTicket').hide()");
+			}
+			System.out.println("El estado seleccionado es el mismo");
+		}
+	}
+
 	public List<Ticket> getTickets() {
 		return tickets;
 	}
@@ -92,28 +128,36 @@ public class AsignacionesBackingBean implements Serializable {
 	public void setTicketController(TicketController ticketController) {
 		this.ticketController = ticketController;
 	}
+
 	public Ticket getTicketSeleccionado() {
 		return ticketSeleccionado;
 	}
+
 	public void setTicketSeleccionado(Ticket ticketSeleccionado) {
 		this.ticketSeleccionado = ticketSeleccionado;
 	}
+
 	public Comentario getComentario() {
 		return comentario;
 	}
+
 	public void setComentario(Comentario comentario) {
 		this.comentario = comentario;
 	}
+
 	public List<Comentario> getComentarios() {
 		return comentarios;
 	}
+
 	public void setComentarios(List<Comentario> comentarios) {
 		this.comentarios = comentarios;
 	}
+
 	@Autowired
 	public void setComentarioController(ComentarioController comentarioController) {
 		this.comentarioController = comentarioController;
 	}
+
 	@Autowired
 	public void setEl(EstadosLoader el) {
 		this.el = el;
@@ -127,7 +171,17 @@ public class AsignacionesBackingBean implements Serializable {
 		this.estadoSeleccionado = estadoSeleccionado;
 	}
 
-	
-	
-	
+	@Autowired
+	public void setTec(TicketEncargadoController tec) {
+		this.tec = tec;
+	}
+
+	public List<TicketEncargado> getAsignados() {
+		return asignados;
+	}
+
+	public void setAsignados(List<TicketEncargado> asignados) {
+		this.asignados = asignados;
+	}
+
 }
--- src/main/java/dsi235/controllers/TicketEncargadoController.java
+++ src/main/java/dsi235/controllers/TicketEncargadoController.java
@@ -1,7 +1,11 @@
 package dsi235.controllers;
 
+import java.util.List;
+
 import dsi235.entities.TicketEncargado;
 
 public interface TicketEncargadoController extends AbstractCrudController<TicketEncargado, Long>{
 
+	public List<TicketEncargado> findByIdTicket_IdTicket(Long idTicket);
+	
 }
