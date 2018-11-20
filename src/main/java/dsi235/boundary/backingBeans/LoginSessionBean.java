51de74b612607db5def0365a1d022ede5bf8c89c
Daniel <daniel.97.molin@gmail.com>
2018-11-20 09:42:49 -0600

    internacionalizacion funciona

--- src/main/java/dsi235/boundary/backingBeans/LoginSessionBean.java
+++ src/main/java/dsi235/boundary/backingBeans/LoginSessionBean.java
@@ -2,9 +2,12 @@ package dsi235.boundary.backingBeans;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Locale;
 
 import javax.annotation.ManagedBean;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import dsi235.controllers.UsuarioController;
@@ -15,7 +18,12 @@ import dsi235.entities.UsuarioRol;
 @SessionScoped
 public class LoginSessionBean implements Serializable {
 
-	public static final long serialVersionUID = 6524787123398319687L;
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private Usuario usuarioLogueado;
 
@@ -24,6 +32,28 @@ public class LoginSessionBean implements Serializable {
 	public void login(String correo, String contrasena) {
 		usuarioLogueado = usuarioController.autenticar(correo, contrasena);
 	}
+	
+	public void changeLanguageUS() {
+		System.out.println("cambiando a ingles");
+		try {
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("eng"));
+		System.out.println(FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void changeLanguageES() {
+		System.out.println("cambiando a espanol");
+		try {
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale("spa"));
+		System.out.println(FacesContext.getCurrentInstance().getViewRoot().getLocale().getLanguage());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 	public boolean isLoggedIn() {
 		return usuarioLogueado != null;
@@ -58,6 +88,8 @@ public class LoginSessionBean implements Serializable {
 		}
 		return false;
 	}
+	
+	
 	
 	
 	
--- src/main/java/dsi235/boundary/sistema/SesionDeUsuarioBean.java
+++ src/main/java/dsi235/boundary/sistema/SesionDeUsuarioBean.java
@@ -16,56 +16,114 @@ import javax.inject.Named;
 @SessionScoped
 public class SesionDeUsuarioBean implements Serializable {
 
-    private String codigoIdioma = "es";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private static Map<String, Object> paises;
+	public void changeLanguageUS(String language) {
+		System.out.println("cambiando a ingles");
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.US);
+	}
+	
+	public void changeLanguageES(String language) {
+		System.out.println("cambiando a espanol");
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(Locale.getDefault());
+	}
 
-    static {
-        paises = new LinkedHashMap<>();
-        paises.put("Español", Locale.forLanguageTag("es"));
-        paises.put("English", Locale.US);
+//	
+	   private String locale;
 
-    }
+	   public static Map<String,Object> countries;
+	   static {
+	        countries = new LinkedHashMap<>();
+	        countries.put("Español", Locale.forLanguageTag("es"));
+	        countries.put("English", Locale.US);
+	    }
 
-    public Map<String, Object> getPaises() {
-        return paises;
-    }
+	   public Map<String, Object> getCountries() {
+	      return countries;
+	   }
 
-    public String getCodigoIdioma() {
-        return codigoIdioma;
-    }
+	   public String getLocale() {
+	      return locale;
+	   }
 
-    public void setCodigoIdioma(String codigoIdioma) {
-        this.codigoIdioma = codigoIdioma;
-    }
+	   public void setLocale(String locale) {
+	      this.locale = locale;
+	   }
 
-    public void cambioDeIdiomaHandler(ValueChangeEvent vce) {
-        if (vce.getNewValue() != null) {
-            try {
-                String nuevoCodigo = vce.getNewValue().toString();
-                for (Map.Entry<String, Object> entrySet : paises.entrySet()) {
-                    Locale value = (Locale) entrySet.getValue();
-                    if (value.toString().compareTo(nuevoCodigo) == 0) {
-                        FacesContext.getCurrentInstance().getViewRoot().setLocale(value);
-                        return;
-                    }
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
-            }
-        }
-    }
+	   //value change event listener
+	   public void localeChanged(ValueChangeEvent e) {
+	      String newLocaleValue = e.getNewValue().toString();
+	      
+	      for (Map.Entry<String, Object> entry : countries.entrySet()) {
+	         
+	         if(entry.getValue().toString().equals(newLocaleValue)) {
+	            FacesContext.getCurrentInstance()
+	               .getViewRoot().setLocale((Locale)entry.getValue());         
+	         }
+	      }
+	   }
+	   
+//	   public void changeLanguage(String language) {
+//	        FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
+//	    }
+	
+	
+	
+	
 
-    public String getMensaje(final String clave) {
-        try {
-            ResourceBundle propiedades = FacesContext.getCurrentInstance().getApplication().getResourceBundle(FacesContext.getCurrentInstance(), "TraduccionesGenerales");
-            if (propiedades != null && propiedades.containsKey(clave)) {
-                return propiedades.getString(clave);
-            }
-        } catch (Exception ex) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
-        }
-        return null;
-    }
+//	private String codigoIdioma = "es";
+//
+//    private static Map<String, Object> paises;
+//
+//    static {
+//        paises = new LinkedHashMap<>();
+//        paises.put("Español", Locale.forLanguageTag("es"));
+//        paises.put("English", Locale.US);
+//
+//    }
+//
+//    public Map<String, Object> getPaises() {
+//        return paises;
+//    }
+//
+//    public String getCodigoIdioma() {
+//        return codigoIdioma;
+//    }
+//
+//    public void setCodigoIdioma(String codigoIdioma) {
+//        this.codigoIdioma = codigoIdioma;
+//    }
+//
+//    public void cambioDeIdiomaHandler(ValueChangeEvent vce) {
+//        if (vce.getNewValue() != null) {
+//            try {
+//                String nuevoCodigo = vce.getNewValue().toString();
+//                for (Map.Entry<String, Object> entrySet : paises.entrySet()) {
+//                    Locale value = (Locale) entrySet.getValue();
+//                    if (value.toString().compareTo(nuevoCodigo) == 0) {
+//                        FacesContext.getCurrentInstance().getViewRoot().setLocale(value);
+//                        return;
+//                    }
+//                }
+//            } catch (Exception ex) {
+//                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+//            }
+//        }
+//    }
+//
+//    public String getMensaje(final String clave) {
+//        try {
+//            ResourceBundle propiedades = FacesContext.getCurrentInstance().getApplication().getResourceBundle(FacesContext.getCurrentInstance(), "TraduccionesGenerales");
+//            if (propiedades != null && propiedades.containsKey(clave)) {
+//                return propiedades.getString(clave);
+//            }
+//        } catch (Exception ex) {
+//            Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+//        }
+//        return null;
+//    }
 
 }
